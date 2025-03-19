b5dac3e6ac2f8de4a03e9927d10b8357eacc26cd
bhanupratap bhana <108392050+bhanupratap17@users.noreply.github.com>
2025-03-19 16:45:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 // f34e0f34
 
-const API_URL = 'http://www.omdbapi.com?apikey=f34e0f34';
+const API_URL = 'https://www.omdbapi.com?apikey=f34e0f34';
        
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
